dcf2e62f8d5379a5e000734c1903ea568ce9fba4
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-09-30 09:49:36 +0100

    Created ascii_robot.py and multiline_message.py files with content

---
+++ basics/output/ascii_robot.py
@@ -0,0 +1,7 @@
+print("Enter character for eye")
+eye = input()
+
+print("#######")
+print(f"# {eye} {eye} #")
+print("# --- #")
+print("#######")
---
+++ basics/output/multiline_message.py
@@ -0,0 +1,6 @@
+print("System Failure Imminent!")
+print("System reboot has been initiated...")
+print("")
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine")
