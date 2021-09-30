ef85ece5e36b035a0880863eabc26d80a2257c02
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-09-30 10:06:12 +0100

    Created file with content.

---
+++ basics/input/string_operators.py
@@ -0,0 +1,12 @@
+print("Enter an amount of lives.")
+lives = int(input())
+
+print("Enter an energy level.")
+energy = int(input())
+
+print("Enter an shield level.")
+shield = int(input())
+
+print(f"Lives Remaining: {'♥' * lives}")
+print(f"Energy Level: {'♦' * energy}")
+print(f"Shield Level: {'♦' * shield}")
