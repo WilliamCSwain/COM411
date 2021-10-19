593c63a91a51bfa55b1e1dc75d06aa84668f0131
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-10-19 13:47:52 +0100

    Amended if activity from "activity" to "calculate"

--- basics/decisions/simple_decision/if_else.py
+++ basics/decisions/simple_decision/if_else.py
@@ -1,7 +1,7 @@
 print("Please enter the activity that you wish to be performed")
 activity= input()
 
-if activity == "activity":
+if activity == "calculate":
     print("Calculations are being performed...")
 else:
     print("Performing the activity...")
