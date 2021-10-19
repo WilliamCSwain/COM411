2af35171b5610e228bc6da0c037e361e76f98b72
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-10-19 13:57:50 +0100

    Reformatted code

--- basics/decisions/simple_decision/if_else.py
+++ basics/decisions/simple_decision/if_else.py
@@ -1,9 +1,9 @@
 print("Please enter the activity that you wish to be performed")
 activity = input()
 
-if activity == "calculate": # if activity is calculate..
-    print("Calculations are being performed...") # Print this!
+if activity == "calculate":  # if activity is calculate..
+    print("Calculations are being performed...")  # Print this!
 else:
-    print("Performing the activity...") # However, if activity = not calculate; print this
+    print("Performing the activity...")  # However, if activity = not calculate; print this
 
-print("Activity has completed.") # Final print task
+print("Activity has completed.")  # Final print task
