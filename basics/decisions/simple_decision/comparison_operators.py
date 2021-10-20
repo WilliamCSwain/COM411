9c5d85f8d107d280a2fb40817eeae49146b2566c
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-10-20 14:16:21 +0100

    Added the if, elif and else statements

--- basics/decisions/simple_decision/comparison_operators.py
+++ basics/decisions/simple_decision/comparison_operators.py
@@ -4,3 +4,9 @@ first_number = int(input())
 print("Please enter the second number")
 second_number = int(input())
 
+if first_number < second_number:
+    print("The first number is the smaller of the two.")
+elif first_number > second_number:
+    print("The second number is the smaller of the two.")
+else:
+    print("The two numbers are the same.")
