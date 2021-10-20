a336870e131d7720938b31a5a2cd415bd3710897
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-10-20 18:12:05 +0100

    Completed code.

--- basics/decisions/simple_decision/counter.py
+++ basics/decisions/simple_decision/counter.py
@@ -9,3 +9,15 @@ third_number = int(input())
 
 even_numbers = 0
 odd_numbers = 0
+
+if first_number % 2 == 0:
+    even_numbers = even_numbers + 1
+else:
+    odd_numbers = odd_numbers + 1
+
+if second_number % 2 == 0:
+    even_numbers = even_numbers + 1
+else:
+    odd_numbers = odd_numbers + 1
+
+print(f"There were {even_numbers} even and {odd_numbers} odd numbers.")
