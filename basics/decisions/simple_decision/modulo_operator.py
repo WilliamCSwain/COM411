e5f3166c236e67455183416ecebd65ab63980fc3
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-10-20 14:00:10 +0100

    Created File, firsts attempt at code executed.

---
+++ basics/decisions/simple_decision/modulo_operator.py
@@ -0,0 +1,7 @@
+print("Please enter a number")
+number = int(input())
+
+if number = 2 == 0:
+    print("The number {number} is an even number.")
+else:
+    print("The number {number} is an odd number.")
