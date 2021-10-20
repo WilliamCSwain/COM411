376749f373cc3630744a3ab91521adbb4b563815
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-10-20 18:02:18 +0100

    Created 1-3 number int variables

---
+++ basics/decisions/simple_decision/counter.py
@@ -0,0 +1,8 @@
+print("Please enter the first number.")
+first_number = int(input())
+
+print("Please enter the second number.")
+second_number = int(input())
+
+print("Please enter the third number.")
+third_number = int(input())
