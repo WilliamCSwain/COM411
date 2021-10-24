6c2abd43a5acc1ab0137bc80b9e7eb18e5315839
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-10-24 20:26:40 +0100

    Amended mistakes

--- basics/decisions/nested_decision/nestception.py
+++ basics/decisions/nested_decision/nestception.py
@@ -2,5 +2,5 @@ print("Where should I look?") # Asks for the place
 place = input()
 
 if place == "in the bedroom":
-    printf("Where in the bedroom should I take a look?")
-    bedroom_place = inputt()
+    print("Where in the bedroom should I take a look?")
+    bedroom_place = input()
