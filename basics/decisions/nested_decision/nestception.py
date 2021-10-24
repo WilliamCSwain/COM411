a780c57f2dc6dec7095c0b149ed62d6a3e41c923
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-10-24 20:31:05 +0100

    Added bedroom_place if and else

--- basics/decisions/nested_decision/nestception.py
+++ basics/decisions/nested_decision/nestception.py
@@ -4,3 +4,8 @@ place = input()
 if place == "in the bedroom":
     print("Where in the bedroom should I take a look?")
     bedroom_place = input()
+
+    if bedroom_place == "under the bed":
+        print("Found shoes, no battery.")
+    else:
+        print("Found mess, no battery.")
