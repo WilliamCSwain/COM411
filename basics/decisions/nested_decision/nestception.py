63d2868d4221710904ba644b528154f403945ff7
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-10-24 20:34:53 +0100

    Added bathroom_place input

--- basics/decisions/nested_decision/nestception.py
+++ basics/decisions/nested_decision/nestception.py
@@ -1,6 +1,7 @@
 print("Where should I look?") # Asks for the place
 place = input()
 
+# Bedroom Checks
 if place == "in the bedroom":
     print("Where in the bedroom should I take a look?")
     bedroom_place = input()
@@ -9,3 +10,8 @@ if place == "in the bedroom":
         print("Found shoes, no battery.")
     else:
         print("Found mess, no battery.")
+
+# Bathroom Checks
+elif plae == "in the bathroom":
+    print("Where in the bathroom should I check?")
+    bathroom_place = input()
