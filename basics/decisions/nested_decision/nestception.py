0fbce6ef5b118ee8d3d88bc591ce2213797fe1e0
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-10-24 20:26:22 +0100

    Added place and bedroom_place inputs

---
+++ basics/decisions/nested_decision/nestception.py
@@ -0,0 +1,6 @@
+print("Where should I look?") # Asks for the place
+place = input()
+
+if place == "in the bedroom":
+    printf("Where in the bedroom should I take a look?")
+    bedroom_place = inputt()
