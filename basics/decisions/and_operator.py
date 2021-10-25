90684b308a5b02bb665b43adf7dfc9992a4a7fc3
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-10-25 21:50:52 +0100

    First attempt completed.

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,10 @@
+print("What was that I just heard?")
+hear = input()
+
+peint("What did I just see?")
+see = input()
+
+if (hear == "grr") and (see == "two red eyes"):
+    print("There is a scary creature. I should probably get out of here..")
+elde:
+    print("I'm a little bit scared... but I'll still continue.")
