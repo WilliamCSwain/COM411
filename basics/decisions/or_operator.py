00d49bf044ed2e59e2e55c6765d3975e56a8da17
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-10-25 21:46:48 +0100

    First submission of code.

---
+++ basics/decisions/or_operator.py
@@ -0,0 +1,9 @@
+print("What type of adventure should I go on?")
+adventure_type = input(!)
+
+if (adventure_type == "scary" ) or (adventure_type == "short"):
+    print("Entering the dark forest..")
+elif (adventure_type == "safe") oe (adventure_type == "long"):
+    print("Taking a safer route..")
+else:
+    print("I'm not too sure which route to take..")
