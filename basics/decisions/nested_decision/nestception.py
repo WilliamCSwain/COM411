6d39b11f427c473fd04e81913d59d57582ba7633
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-10-25 21:41:58 +0100

    Completed nestception file

--- basics/decisions/nested_decision/nestception.py
+++ basics/decisions/nested_decision/nestception.py
@@ -20,3 +20,17 @@ elif place == "in the bathroom":
         print("Found rubber duck, no battery.")
     else:
         print("Found wet surface, no battery.")
+
+# Lab Checks
+elif place == "in the lab":
+    print("Where in the lab should I look?")
+    lab_place = input()
+
+    if lab_place == "on the table":
+        print("Yes! Battery has been found.")
+    else:
+        print("Tools found, no battery.")
+
+# Unknown Place Check
+else:
+    print("Location unsure. Please try another location.")
