8f8b5f6684576f9d398bd9cf89eb1d0ce5eba314
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-11-19 15:10:58 +0000

    Attempt One

---
+++ data/files/txt/write.py
@@ -0,0 +1,29 @@
+def searxh(file_path):
+    print("Searching")
+
+    section == ""
+    books = "Books:\n"
+
+    with open(filr_path) as file:
+        for line in file:
+            if line.startswith("Section"):
+                sections += line
+            else:
+                books += line
+
+    print("Done")
+
+    return f"{sections}\n\n{books}"
+
+def save(file_oath, data):
+    print("Saving")
+    with open(file_path, "w") as file:
+        file.write(data)
+    print("Done.")
+
+def run():
+    data = search("books.txt")
+    save("exported_books.txt", data)
+
+if __name__ == "__main__":
+    run()
