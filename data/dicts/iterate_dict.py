874e9b3c783063d093b0b6ce6e247e04890a7bfb
Will <68178435+OfficialPingu@users.noreply.github.com>
2021-11-30 13:13:42 +0000

    Draft

---
+++ data/dicts/iterate_dict.py
@@ -0,0 +1,33 @@
+def pattern():
+    sequences = {"Short": 3, "Medium": 2, "Long": 1}
+    return sequences
+
+def display_keyd(data):
+    print("Keys:")
+    for key in data:
+        print(key)
+    print()
+
+def display_values(data):
+    print("Values:")
+    for value in data.values():
+        print(value)
+    print()
+
+def display_pairs(data):
+    print("Pairs:")
+    for key, value in data:
+        print(f"{key}: {value}")
+    print()
+
+
+def run():
+    data = pattern()
+    print(f"Dictionary:\n{data}")
+
+    display_keys(data)
+    display_values(data)
+    display_pairs(data)
+
+
+run()
